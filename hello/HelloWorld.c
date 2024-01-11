745b0f18539bb072d50f908b16fd12aba53e4b0c
wangtao <eatfirewood@gmail.com>
2024-01-11 16:56:12 +0800

    test

--- hello/HelloWorld.c
+++ hello/HelloWorld.c
@@ -6,6 +6,7 @@ int main()
 {
    int a = 4;
    int b = 5;
-   printf("Calling assembly function my_function with x0=%d and x1=%d results in %d\n", a, b, my_function(a, b));
+   printf("Calling assembly function my_function with x0=%d and\
+ x1=%d results in %d\n", a, b, my_function(a, b));
    return (0);
 }
